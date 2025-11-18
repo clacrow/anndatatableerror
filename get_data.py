a5529cd420a8d59b46dd962e8b8970c61ecef13a
Pawel Rzonca <pawel.rzonca@contractors.roche.com>
2025-11-18 20:48:10 +0100

    even smaller

--- get_data.py
+++ get_data.py
@@ -3,13 +3,9 @@ from pathlib import Path
 import numpy as np
 from ngio import (
     Image,
-    ImageInWellPath,
     Label,
     OmeZarrContainer,
-    OmeZarrPlate,
-    Roi,
     create_empty_ome_zarr,
-    create_empty_plate,
     open_ome_zarr_container,
 )
 from ngio.utils import download_ome_zarr_dataset
@@ -18,16 +14,8 @@ from ngio.utils import download_ome_zarr_dataset
 
 ROOT_PATH: Path = Path("./data")
 
-TARGET_DATASET_PATH: Path = ROOT_PATH / "test_data.zarr"
-TARGET_DATASET_NAME: str = "Test Dataset"
-
-
 DOWNLOAD_DIR_PATH: Path = ROOT_PATH / "downloaded_dataset"
-DOWNLOAD_PATH: Path = (
-    DOWNLOAD_DIR_PATH / "20200812-CardiomyocyteDifferentiation14-Cycle1-small.zarr"
-)
 
-print("Downloading source dataset...")
 source_hcs_path = download_ome_zarr_dataset(
     "CardiomyocyteSmall",
     download_dir=ROOT_PATH / "downloaded_dataset",
@@ -39,21 +27,12 @@ print(f"{source_ome_zarr=}")
 source_image: Image = source_ome_zarr.get_image("2")
 print(f"{source_image=}")
 
-image_roi: Roi = source_ome_zarr.build_image_roi_table().rois()[0]
-
-target_plate_wells = [ImageInWellPath(row="A", column="01", path="0")]
-test_3d_plate: OmeZarrPlate = create_empty_plate(
-    store=TARGET_DATASET_PATH,
-    name=TARGET_DATASET_NAME,
-    images=target_plate_wells,
-    overwrite=True,
-)
 
 # prepare target container
-source_array: np.ndarray = source_image.get_roi(image_roi)
+source_array: np.ndarray = source_image.get_array()
 
 target_container = create_empty_ome_zarr(
-    TARGET_DATASET_PATH / "A/01/0",
+    ROOT_PATH / "test_data.zarr/A/01/0",
     shape=source_array.shape,
     xy_pixelsize=source_image.pixel_size.x,
     levels=3,
@@ -70,7 +49,7 @@ source_label: Label = source_ome_zarr.get_label(
     "nuclei",
     pixel_size=source_image.pixel_size,
 )
-source_label_array: np.ndarray = source_label.get_roi(image_roi)
+source_label_array: np.ndarray = source_label.get_array()
 target_label: Label = target_container.derive_label(
     name="nuclei",
     overwrite=True,
