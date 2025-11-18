a83ee3301693c00c99bac96bc91db3cf0651e4aa
Pawel Rzonca <pawel.rzonca@contractors.roche.com>
2025-11-18 20:53:24 +0100

    more clear

--- read_roi_table.py
+++ read_roi_table.py
@@ -3,4 +3,7 @@ from ngio import open_ome_zarr_container
 ome_zarr = open_ome_zarr_container("data/test_data.zarr/A/01/0")
 
 table = ome_zarr.get_generic_roi_table("nuclei_ROI_table")
-print(f"{table=}")
+print("table as dataframe:")
+print(f"{table.dataframe=}")
+print("table rois:")
+print(f"{table.rois()=}")
